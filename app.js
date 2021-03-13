0c485c9e5e6090452b9830ff3c9ddcc7df329f55
Ross Foy <hamzaoflouisville@gmail.com>
2021-03-13 02:24:15 -0500

    make the app more modular by placing routes in a separate folder in the project with appropriate 'module.exports' and 'require()' usage.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const Sequelize = require('sequelize');
 const application = express();
+const mainRoutes = require('./routes');
 
-application.get('/', (req, res) => {
-    res.send('<em>This is one funky test.</em>');
-});
+application.use(mainRoutes);
 
 application.listen(3000, () => {
     console.log('We are tuning into Localhost 3000, the hottest station in town!');
