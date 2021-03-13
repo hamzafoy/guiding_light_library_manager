42744f3fe19703288d78a36dcfbec664226a3f41
Ross Foy <hamzaoflouisville@gmail.com>
2021-03-13 04:17:04 -0500

    create a database with the name of 'library' and successfully test connection inside an async function in app.js

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ const Sequelize = require('sequelize');
 const application = express();
 const mainRoutes = require('./routes');
 
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'library.db',
+    logging: false,
+});
+
 application.use(mainRoutes);
 
 application.listen(3000, () => {
     console.log('We are tuning into Localhost 3000, the hottest station in town!');
 });
+
+(async () => {
+
+    try {
+        await sequelize.authenticate();
+        console.log('Airlock to Database is secure. . .100%');
+    } catch (error) {
+        console.error('Unfortunately, the airlock is not air tight and our connection to the database has failed', error);
+    }
+
+})();
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.send('<em>This is one funky test. My routes are now modular - check them out.</em>');
 });
 
+router.get('/library', (req, res) => {
+    res.send('<strong>Testing a separate route.</strong>');
+});
+
 module.exports = router;
